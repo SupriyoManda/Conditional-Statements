b69344c91ea5b0cb0b0099c569d2d5453d9f37a3
Supriyo Mandal <supriyo.mandal27@gmail.com>
2024-09-11 02:27:09 +0530

    ACP

---
+++ Summer times.py
@@ -0,0 +1,8 @@
+temp = float(input("Enter the temperature in Celsius: "))
+if temp > 19:
+  print('Wear half sleeves and shorts')
+elif temp < 9:
+  print('Wear a jacket and pants')
+else:
+  print('Wear a full sleeve shirt and pants')
+        
