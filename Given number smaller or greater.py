bb4b3c024c29bacd8598639751ca16f5a5f08527
Supriyo Mandal <supriyo.mandal27@gmail.com>
2024-09-11 02:22:23 +0530

    Greater number

---
+++ Given number smaller or greater.py
@@ -0,0 +1,14 @@
+number = int(input('Enter a number: '))
+
+if number > 400:
+
+   print('The number you entered is greater than 400.')
+
+elif number < 400:
+
+   print('The number you entered is less than 400.')
+
+else:
+
+   print('The number you entered is equal to 400.')
+
